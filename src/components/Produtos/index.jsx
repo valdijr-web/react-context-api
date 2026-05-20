5ab7565b17898c1d453b79a32e19eaa6f11fe436
valdijunior <valdi.ads@gmail.com>
2026-05-20 19:51:36 -0300

    feat: cria hook para gerenciar estado do carrinho do contexto e funções de manipulação.

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -4,47 +4,10 @@ import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
 import { useContext } from "react";
 import { CarrinhoContext } from "@/context/CarrinhoContext";
+import { useCarrinhoContext } from "@/hooks/useCarrinhoContext";
 
 const Produtos = () => {
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
-
-  function adicionarProduto(novoProduto) {
-    const temOProduto = carrinho.some((itemDoCarrinho) => { itemDoCarrinho.id === novoProduto.id })
-    if (!temOProduto) {
-      novoProduto.quantidade = 1;
-      return setCarrinho((carrinhoAnterior) => [
-        ...carrinhoAnterior,
-        novoProduto
-      ]);
-    }
-
-    setCarrinho((carrinhoAnterior) => carrinhoAnterior.map((itemDoCarrinho) => {
-      if (itemDoCarrinho.id === novoProduto.id) itemDoCarrinho.quantidade += 1;
-      return itemDoCarrinho;
-    }));
-  }
-
-  function removerProduto(id) {
-    // encontra o produto em questão
-    const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
-    // verifica se a quantidade é igual a um. Isso significa que este é o último 
-    // produto do tipo no carrinho
-    const ehOUltimo = produto.quantidade === 1;
-    // Com o if faz a verificação do último produto do tipo no carrinho e atualiza o estado do carrinho
-    if (ehOUltimo) {
-      return setCarrinho((carrinhoAnterior) =>
-        carrinhoAnterior.filter((itemDoCarrinho) => itemDoCarrinho.id !== id)
-      );
-    }
-    // Se não é o último produto do carrinho, só atualizamos a quantidade, removendo um item
-    setCarrinho((carrinhoAnterior) =>
-      carrinhoAnterior.map((itemDoCarrinho) => {
-        if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade -= 1;
-        return itemDoCarrinho;
-      })
-    );
-  }
-
+  const { adicionarProduto } = useCarrinhoContext();
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
