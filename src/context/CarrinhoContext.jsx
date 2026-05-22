3404ddf64132a6b412670e74827b26732e9f8fbf
valdijunior <valdi.ads@gmail.com>
2026-05-22 16:12:36 -0300

    feat: calcula valor total e quantidade total de produtos do carrinho.

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -2,12 +2,24 @@ import { useState } from "react";
 import { createContext } from "react";
 
 export const CarrinhoContext = createContext();
+CarrinhoContext.displayName = "CarrinhoContext";
 
 export const CarrinhoProvider = ({ children }) => {
     const [carrinho, setCarrinho] = useState([]);
+    const [quantidade, setQuantidade] = useState(0);
+    const [valorTotal, setValorTotal] = useState(0);
 
     return (
-        <CarrinhoContext.Provider value={{carrinho, setCarrinho}}>
+        <CarrinhoContext.Provider
+            value={{
+                carrinho,
+                setCarrinho,
+                quantidade,
+                setQuantidade,
+                valorTotal,
+                setValorTotal
+            }}
+        >
             {children}
         </CarrinhoContext.Provider>
     );
